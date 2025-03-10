c12a5787b28d987e1ba9ce61c95d31d624ea0a64
P3dr0S17v5 <pedro.graffic@gmail.com>
2025-03-10 15:19:48 +0000

    Classe Associacao OrderItem

--- src/main/java/com/infordevtech/dscommerce/entities/Product.java
+++ src/main/java/com/infordevtech/dscommerce/entities/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,12 +31,14 @@ public class Product {
 	
 	private String imgUrl;
 	
-	
 	@ManyToMany
 	@JoinTable( name = "tb_produtc_category", 
 		joinColumns = @JoinColumn(name = "product_id"),
 		inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product () {
 	}
@@ -93,7 +96,11 @@ public class Product {
 		return categories;
 	}
 
-	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+
 
 	
 	
