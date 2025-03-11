dda3e7d8770260f63eb9eaca68f3a023241b7330
P3dr0S17v5 <pedro.graffic@gmail.com>
2025-03-11 14:55:26 +0000

    Seeding da base de dados

--- src/main/java/com/infordevtech/dscommerce/entities/Product.java
+++ src/main/java/com/infordevtech/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.infordevtech.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -32,7 +33,7 @@ public class Product {
 	private String imgUrl;
 	
 	@ManyToMany
-	@JoinTable( name = "tb_produtc_category", 
+	@JoinTable( name = "tb_product_category", 
 		joinColumns = @JoinColumn(name = "product_id"),
 		inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
@@ -100,8 +101,9 @@ public class Product {
 		return items;
 	}
 
-
-
+	public List<Order> getOrders() {
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
 	
 	
 }
