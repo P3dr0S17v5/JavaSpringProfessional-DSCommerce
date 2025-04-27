8e142c1678bd5e290a01deca1e6a0a0f206297e2
P3dr0S17v5 <pedro.graffic@gmail.com>
2025-04-27 18:43:35 +0100

    Post new product

--- src/main/java/com/infordevtech/dscommerce/controllers/ProductController.java
+++ src/main/java/com/infordevtech/dscommerce/controllers/ProductController.java
@@ -1,12 +1,18 @@
 package com.infordevtech.dscommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.infordevtech.dscommerce.dto.ProductDTO;
 import com.infordevtech.dscommerce.services.ProductService;
@@ -19,13 +25,24 @@ public class ProductController {
 	private ProductService service;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO finById(@PathVariable Long id) {
-		return service.findById(id);	
+	public ResponseEntity<ProductDTO> finById(@PathVariable Long id) {
+		ProductDTO  dto = service.findById(id);	
+		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
-	public Page<ProductDTO> finAll(Pageable pageable) {
-		return service.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> finAll(Pageable pageable) {
+		Page<ProductDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);	
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
 		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
